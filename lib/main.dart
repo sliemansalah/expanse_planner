67f72c2ff2584a454578f5a49df5301605322fa2
slieman <slieman2001@hotmail.com>
2020-03-20 19:33:47 +0200

    Styling Text

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,14 @@ class MyHomePage extends StatelessWidget {
                             width:2
                           )),
                           padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString()),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
+                        ),
                     ),
                     Column(
                       children: <Widget>[
