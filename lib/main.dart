127da5c0952a470b2a8b24c7fcfe11281cdccecd
slieman <slieman2001@hotmail.com>
2020-03-20 19:23:57 +0200

    add Transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyHomePage());
 
 class MyHomePage extends StatelessWidget {
+  
+  final List<Transaction> transactions = [
+    Transaction(id:'t1',title:'New Shoes', amount: 69.69, date: DateTime.now(),),
+        Transaction(id:'wed7',title:'Weekly dat', amount: 16.53, date: DateTime.now(),)
+
+  ]; 
+
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
