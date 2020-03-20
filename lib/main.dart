ee5fea206d207cd6b1b14e54580b062a5500ad7e
slieman <slieman2001@hotmail.com>
2020-03-20 19:31:51 +0200

    Styling a Container

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyHomePage());
 
 class MyHomePage extends StatelessWidget {
-  
   final List<Transaction> transactions = [
-    Transaction(id:'t1',title:'New Shoes', amount: 69.69, date: DateTime.now(),),
-        Transaction(id:'wed7',title:'Weekly Georacy', amount: 16.53, date: DateTime.now(),)
-
-  ]; 
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.69,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'wed7',
+      title: 'Weekly Georacy',
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
 
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,21 +37,32 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(children: transactions.map((tx){
-            return Card(
-              child: Row(children: <Widget>[
-                Container(
-                  child: Text(
-                    tx.amount.toString()
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(border: Border.all(
+                            color:Colors.black,
+                            width:2
+                          )),
+                          padding: EdgeInsets.all(10),
+                      child: Text(tx.amount.toString()),
                     ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString())
+                      ],
+                    )
+                  ],
                 ),
-                Column(children: <Widget>[
-                  Text(tx.title),
-                  Text(tx.date.toString())
-                ],)
-              ],),
-            );
-          }).toList(),)
+              );
+            }).toList(),
+          )
         ],
       ),
     ));
